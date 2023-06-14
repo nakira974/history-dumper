d3e3ee609d81028842295aa22e0cbe8676333526
nakira974 <maxime.loukhal@gmail.com>
2023-06-14 17:11:15 +0200

    Add SessionProvider.tsx

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,12 +4,15 @@ import KeycloakProvider from "next-auth/providers/keycloak"
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export const authOptions: NextAuthOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
     // https://next-auth.js.org/configuration/providers/oauth
     providers: [
         KeycloakProvider({
-            clientId: process.env.KEYCLOAK_CLIENT_ID!.toString(),
-            clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!.toString(),
-            issuer: process.env.KEYCLOAK_ISSUER!.toString()!,
+            clientId: process.env.KEYCLOAK_CLIENT_ID as string,
+            clientSecret: process.env.KEYCLOAK_CLIENT_SECRET as string,
+            issuer: process.env.KEYCLOAK_ISSUER as string,
+            accessTokenUrl :  process.env.KEYCLOAK_ACCESS_TOKEN_URL as string,
+            profileUrl : process.env.KEYCLOAK_PROFILE_URL as string,
         })
     ],
     callbacks: {
